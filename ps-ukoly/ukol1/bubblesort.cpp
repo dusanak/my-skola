54a9b92e7a13b042b94b3aa41d22f39db5717d03
dusanak <dusan.janis97@gmail.com>
2020-09-20 13:18:59 +0200

    Cleaned up ukol1 in ps-ukoly

--- ps-ukoly/ukol1/bubblesort.cpp
+++ ps-ukoly/ukol1/bubblesort.cpp
@@ -7,22 +7,20 @@ void bubblesort(std::vector<int> & data);
 int main() {
     std::vector<int> data = std::vector<int>();
 
-    for (int i = 0; i < 20000; i++) {
-        data.push_back(std::rand() % 10000);
+    for (int i = 0; i < 50; i++) {
+        data.push_back(std::rand() % 100);
     }
 
     for (int i = 0; i < 50; i++) {
         std::cout << data[i] << " ";
     }
-
     std::cout << std::endl;
 
     bubblesort(data);
 
-    for (int i = 0; i < 50; i++) {
+    for (size_t i = 0; i < 50; i++) {
         std::cout << data[i] << " ";
     }
-
     std::cout << std::endl;
 
     return 1;
@@ -33,8 +31,8 @@ void bubblesort(std::vector<int> & data) {
 
     while (flag) {
         flag = false;
-        for (int i = 0; i < data.size(); i++) {
-            if ((i + 1 < data.size()) && (data[i] > data[i + 1])) {
+        for (size_t i = 0; i + 1 < data.size(); i++) {
+            if (data[i] > data[i + 1]) {
                 int tmp = data[i];
                 data[i] = data[i + 1];
                 data[i + 1] = tmp;
