359a7a57c236e2a009602363043f4e594bf83cc5
Dusan <dusan.janis97@gmail.com>
2020-09-22 14:46:01 +0200

    Cleared up pp-ukoly and added a few functions

--- pp-ukoly/ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for haskell-skola
-
-## Unreleased changes
---
+++ pp-ukoly/exp.hs
@@ -0,0 +1,13 @@
+
+sumCustom :: Int -> Int -> Int
+sumCustom x y = x + y
+
+
+fact :: Int -> Int
+fact 0 = 1
+fact x = x * fact (x - 1)
+
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib x = fib (x - 1) + fib (x - 2)
