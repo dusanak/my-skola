547fd1eb32cabf81d62b770d51c7e35683172554
dusanak <dusan.janis97@gmail.com>
2020-10-21 01:19:52 +0200

    Finished task5, cleaned up task5 directory

---
+++ ps-ukoly/speed_comparison/script.py
@@ -0,0 +1,24 @@
+import os
+from string import Template
+import timeit
+
+def measureTimeToCSV():
+    code = Template("""
+import os
+os.system("{ ./speed_comparison/$exec $len $thr; } 1>/dev/null")
+""")
+
+    executable = "task5"
+    length = 6
+    threads = 12
+
+    with open("./speed_comparison/times.csv", "a") as f:
+        f.write("Name: {}\n".format(executable))
+        f.write("Znaky: {}\n".format(length))
+        f.write("---------------------------\n")
+        f.write("FORKS        TIME\n")
+        f.write("{:2d}           {:f}\n".format(threads, timeit.timeit(code.substitute(exec = executable, len = str(length), thr=str(threads)), number=1)))
+        f.write("\n")
+
+if __name__ == "__main__":
+    measureTimeToCSV()
--- ps-ukoly/ukol5/task5.cpp
+++ ps-ukoly/ukol5/task5.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <iomanip>
-#include <vector>
 #include <unistd.h> 
 #include <sys/wait.h>
 #include <openssl/evp.h>
@@ -11,13 +10,9 @@
 #define ALPHABET_SIZE 26
 
 std::string numberToString(int input_number);
-void incrementString(std::string & input_string, int amount);
-char incrementLetter(char input_char, int amount);
-char base26toASCII(unsigned int input_number);
-int asciiToBase26(char input_char);
+void incrementString(std::string & input_string);
 std::string convertStringToMD5(std::string input_string);
 void convertToMD5Parallel();
-void convertToMD5ParallelAlt();
 
 int STRING_LENGTH, NUMBER_OF_PROCESSES;
 
@@ -32,20 +27,12 @@ int main(int argc, char *argv[]) {
 
     std::cout << "String length: " << STRING_LENGTH << "\n" << "Number of processes: " << NUMBER_OF_PROCESSES << "\n" << std::endl;
     
-    convertToMD5ParallelAlt();
+    convertToMD5Parallel();
 
     return 0;
 }
 
-//Pracujeme s abecedou a-z
-char base26toASCII(unsigned int input_number) {
-    return char(int('a') + input_number);
-}
-
-int asciiToBase26(char input_char) {
-    return int(input_char) - int('a');
-}
-
+//prevod retezce na vstupu do MD5 s vyuzitim OpenSSL
 std::string convertStringToMD5(std::string input_string) {
     unsigned char md_value[EVP_MAX_MD_SIZE];
     unsigned int md_length;
@@ -56,7 +43,7 @@ std::string convertStringToMD5(std::string input_string) {
     EVP_DigestFinal(mdctx, md_value, &md_length);
     EVP_MD_CTX_destroy(mdctx);
 
-    //převod z unsigned char * na std::string
+    //prevod z unsigned char * na std::string
     std::stringstream ss;
     ss << std::hex << std::setfill('0');
     for (size_t i = 0; i < md_length; i++) {
@@ -66,26 +53,26 @@ std::string convertStringToMD5(std::string input_string) {
     return ss.str();
 }
 
-void incrementString(std::string & input_string, int amount) {
-    if (input_string.length() == 0) {
-        return;
-    }
-
-    for (int i = input_string.length() - 1; i >= 0; i--){
-        if (amount == 0) {
+//prevede retezec na dalsi retezec v poradi
+void incrementString(std::string & input_string) {
+    for (int i = STRING_LENGTH - 1; i >= 0; i--) {
+        if (++input_string[i] > int('z')) {
+            input_string[i] = 'a';
+        } else {
             return;
         }
-
-        char original_letter = input_string[i];
-        input_string[i] = incrementLetter(original_letter, amount % ALPHABET_SIZE);
-        amount = (amount + asciiToBase26(original_letter)) / ALPHABET_SIZE;
     }
 }
 
-char incrementLetter(char input_char, int amount) {
-    return base26toASCII((asciiToBase26(input_char) + amount) % ALPHABET_SIZE);
+//prevede cislo na ekvivalentni retezec
+std::string numberToString(int input_number) {
+    std::string output_string = std::string(STRING_LENGTH, ' ');
+    for (int i = STRING_LENGTH - 1; i >= 0; i--) {
+        output_string[i] = 'a' + (input_number % ALPHABET_SIZE);
+        input_number = input_number / ALPHABET_SIZE;
+    }
+    return output_string;
 }
-
 
 void convertToMD5Parallel() {
     size_t size_of_block = int(pow(ALPHABET_SIZE, STRING_LENGTH) + 0.5) / NUMBER_OF_PROCESSES;
@@ -93,57 +80,16 @@ void convertToMD5Parallel() {
     for (int i = 0; i < NUMBER_OF_PROCESSES; i++) {
         int pid = fork();
         if (pid == 0) {
-            std::string current_string = std::string(STRING_LENGTH, 'a');
-            incrementString(current_string, i * size_of_block);
-
+            std::string current_string = numberToString(i * size_of_block);
+        
+            //posledni blok prace je vetsi aby byly zpracovany vsechny retezce
             if (i == NUMBER_OF_PROCESSES - 1) {
                 size_of_block += int(pow(ALPHABET_SIZE, STRING_LENGTH) + 0.5) % size_of_block;
             }
 
             for (size_t j = 0; j < size_of_block; j++) {
-                std::cout << current_string << ":" << convertStringToMD5(current_string) << " ";
-                incrementString(current_string, 1);
-            }
-            std::cout << std::endl;
-
-            exit(0);
-        }
-    }
-
-    while (waitpid(-1, NULL, 0) > 0) {}
-}
-
-//Alternative
-std::string numberToString(int input_number) {
-    std::string output_string = std::string(STRING_LENGTH, ' ');
-    for (int i = STRING_LENGTH - 1; i >= 0; i--) {
-        output_string[i] = base26toASCII(input_number % int(pow(ALPHABET_SIZE, STRING_LENGTH - i + 1) + 0.5));
-        input_number = input_number % ALPHABET_SIZE;
-    }
-    return output_string;
-}
-
-//Alternative
-void convertToMD5ParallelAlt() {
-    size_t size_of_block = int(pow(ALPHABET_SIZE, STRING_LENGTH) + 0.5) / NUMBER_OF_PROCESSES;
-
-    int first_string = 0;
-
-    for (int i = 0; i < STRING_LENGTH; i++) {
-        first_string += int(pow(ALPHABET_SIZE, i) + 0.5);
-    }
-
-    for (int i = 0; i < NUMBER_OF_PROCESSES; i++) {
-        int pid = fork();
-        if (pid == 0) {
-            int current_string = first_string + size_of_block * i;
-
-            if (i == NUMBER_OF_PROCESSES - 1) {
-                size_of_block += int(pow(ALPHABET_SIZE, STRING_LENGTH) + 0.5) % size_of_block;
-            }
-
-            for (size_t j = 0; j < size_of_block; j++) {
-                std::cout << numberToString(current_string) << ":" << convertStringToMD5(numberToString(current_string)) << " ";
+                std::cout << convertStringToMD5(current_string) << " ";
+                incrementString(current_string);
             }
             std::cout << std::endl;
 
