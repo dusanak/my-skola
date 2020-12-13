b67b76c34197f977a4c8c53f2c53afafa6f0163f
dusanak <dusan.janis97@gmail.com>
2020-12-13 09:31:41 +0100

    Added an exampleObject and a class to go with it and a exampleTypeChecks

---
+++ pp-ukoly/kotlin/src/kotlin_example/KotlinExampleClass.kt
@@ -0,0 +1,31 @@
+package kotlin_example
+
+// Interfaces work the same way as they do in Java. All classes inheriting from interfaces must implement
+// the interface's methods.
+interface Shape {
+    fun area(): Int
+    fun draw(): String
+}
+
+// Kotlin classes can have several constructors.
+// Every class must have a primary constructor. Primary constructor is a part of the header.
+// There can also be any number of secondary constructors.
+// Constructor parameters with a var or val prefix are also class fields.
+open class Rectangle(val x: Int, val y: Int): Shape {
+
+    override fun area(): Int {
+        return x * y
+    }
+
+    override fun draw(): String {
+        var picture = ""
+        picture += "*".repeat(x) + "\n"
+        picture += ("*" + " ".repeat(x - 2) + "*" + "\n").repeat(y - 2)
+        picture += "*".repeat(x)
+        return picture
+    }
+}
+
+// This is a child class. Classes can only inherit from open classes.
+class Square(a: Int) : Rectangle(a, a)
+
--- pp-ukoly/kotlin/src/kotlin_example/KotlinExamples.kt
+++ pp-ukoly/kotlin/src/kotlin_example/KotlinExamples.kt
@@ -1,23 +1,30 @@
 package kotlin_example
 
-// All helper functions are written in Java.
+// Helper functions are written in Java.
 // This is to demonstrate the full interoperability of Java and Kotlin.
 
 import java_example.HelperFunctions.randomEvenInt
 import java_example.HelperFunctions.randomEvenIntList
 
-
 // In contrast with Java, Kotlin allows top-level functions. During compilation, Kotlin compiler creates a Java class
-// and converts all these functions into static functions.
+// and converts all top-level functions into this class's static functions.
 fun main(args : Array<String>) {
+    exampleMutable()
+    println()
+    exampleNullable()
+    println()
     exampleFunctional()
+    println()
+    exampleObject()
+    println()
+    exampleTypeChecks()
 }
 
 // Kotlin differentiates between immutable and mutable variables.
 // An immutable variable is declared using the val keyword.
 // A mutable one is declared using the keyword var.
 fun exampleMutable() {
-    // Val keyword is equivalent to the final keyword in Java. Such a variable is immutable.
+    // Val keyword is equivalent to the final keyword in Java. Such a variable is immutable after a value is assigned.
     val finalInt: Int
 
     // Can assign a value the first time.
@@ -25,7 +32,7 @@ fun exampleMutable() {
     println(finalInt)
 
     // Will not compile, since the variable is immutable.
-    // finalVariable += 2
+    // finalInt += 2
 
     // Var keyword makes the variable mutable.
     var mutableInt = finalInt
@@ -71,4 +78,68 @@ fun exampleFunctional() {
 
     // SortedBy return a sorted list using a comparison function.
     println(randomIntList.sortedBy { it ?: 0 })
+}
+
+// Because Kotlin is an object-oriented language, it must have classes and objects.
+// Kotlin has a bit different syntax when it comes to classes.
+fun exampleObject() {
+    val square = Square(4)
+
+    // Kotlin supports the creation of extension functions, which are defined outside the class itself,
+    // in contrast to regular class methods. These functions can access only of class's public fields and interface.
+    // The receiving class is not actually modified, however dot-notation is used to call extension methods,
+    // which improves code clarity.
+    fun Rectangle.circumference(): Int {
+        return this.x * 2 + this.y * 2
+    }
+    println(square.circumference())
+}
+
+// Kotlin has a nice way of handling type checking and casting.
+// Using a keyword "is", Kotlin performs a type check and in the subsequent block,
+// the variable is safely cast to the appropriate type.
+// There is also an unsafe way to cast types and that is the keyword "as".
+fun exampleTypeChecks() {
+    // This might seem to be a raw list. Kotlin however does not have raw collection at all unlike Java.
+    // Usually when creating a list, a type should included. But because the list is initialized with values,
+    // Kotlin can infer its type.
+    val objects = listOf(null, 7, 4, "Test", Rectangle(5, 4), Square(3))
+
+    objects.forEach { obj ->
+        if (obj == null) {
+            println("Object is $obj")
+        }
+
+        if (obj is Int) {
+            println("$obj is " + if ((obj % 2) == 0) "even" else "odd")
+        }
+
+        if (obj is String) {
+            println(obj)
+        }
+
+        // There even is a "!is" which checks if an object is not of a type.
+        // It still correctly casts to the appropriate type in the else block.
+        if (obj !is Shape) {
+            println("Not a shape")
+        } else {
+            println(obj.draw())
+        }
+    }
+
+    // We can override type inference by explicitly stating the variable's type.
+    val x: Any = 5
+
+    // Will not compile
+    // println(x + 10)
+
+    // Throws exception at runtime because Int cannot be cast to Float
+    try {
+        println(x as Float)
+    } catch (e: Exception) {
+        println(e)
+    }
+
+    // In this case, the type cast works.
+    println(x as Int + 10)
 }
