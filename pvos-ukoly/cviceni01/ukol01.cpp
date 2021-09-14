1657ed1477502d4b7f667f7ee239a6cf63af2f69
Dusan J <dusan.janis97@gmail.com>
2021-09-14 17:16:41 +0200

    Final version of cviceni01

--- pvos-ukoly/cviceni01/ukol01.cpp
+++ pvos-ukoly/cviceni01/ukol01.cpp
@@ -19,8 +19,14 @@ int main() {
     while ((pid = waitpid(-1, &status, 0)) > 0) {
         if (WIFEXITED(status)) {
             printf("Process with pid %d exited with exit status %d.\n", pid, WEXITSTATUS(status));
-        } else {
-            printf("Process with pid %d did not terminate normally.\n", pid);
+        } else if (WIFSIGNALED(status)) {
+            if (WCOREDUMP(status)) {
+                printf("Process with pid %d was terminated by signal %d and produced a core dump.\n", pid, WTERMSIG(status));
+            } else {
+                printf("Process with pid %d was terminated by signal %d.\n", pid, WTERMSIG(status));
+            }
+        } else if (WIFSTOPPED(status)) {
+            printf("Process with pid %d was stopped by signal %d.\n", pid, WSTOPSIG(status));
         }
     }    
 
@@ -31,6 +37,8 @@ int childProcess(int number) {
     if ((number % 3) == 0) {
         return number;
     } else if ((number % 7) == 0) {
+        return number / 0;
+    } else if ((number % 11) == 0) {
         getc(nullptr);
         return 1;
     } else {
--- pvos-ukoly/cviceni01/ukol02_withcounter.cpp
+++ pvos-ukoly/cviceni01/ukol02_withcounter.cpp
@@ -3,22 +3,30 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#define NUMBER_OF_PROCESSES 10 
+
 int childProcess(int time_to_run);
 
 int main() {
+    int number_of_running_processes = 0;
     while (true) {
-        int pid, status, random_number;
+        while (number_of_running_processes < NUMBER_OF_PROCESSES) {
+            int random_number = rand() % 20;
+
+            number_of_running_processes += 1;
+            int pid = fork();
         
-        random_number = rand() % 20;
-        pid = fork();
-    
-        if (pid == 0) {
-            return childProcess(random_number);
+            if (pid == 0) {
+                return childProcess(random_number);
+            }
         }
 
         sleep(1);
         
+        int status, pid;
         while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+            number_of_running_processes -= 1;
+
             if (WIFEXITED(status)) {
                 printf("Process with pid %d exited with exit status %d.\n", pid, WEXITSTATUS(status));
             } else {
