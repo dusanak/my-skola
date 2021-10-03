5401a8ee06728f18935c474194469aa9ae8e33cf
Dusan J <dusan.janis97@gmail.com>
2021-10-03 22:02:27 +0200

    Added a multithreaded implementation of TSP in Rust

--- pai-ukoly/cviceni02/rust/src/main.rs
+++ pai-ukoly/cviceni02/rust/src/main.rs
@@ -1,6 +1,14 @@
+use std::time::Instant;
+use std::time::Duration;
+use std::collections::VecDeque;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::sync::mpsc;
+use std::thread;
 use itertools::Itertools;
 
-const MAX_POSITIONS: usize = 10;
+const MAX_POSITIONS: usize = 12;
+const MAX_THREADS: usize = 1;
 
 struct Point(f32, f32);
 
@@ -27,8 +35,81 @@ fn brute_force_tsp(distance_matrix: &Vec<Vec<f32>>) -> (Vec<usize>, f32) {
         if current_shortest_distance < shortest_distance {
             shortest_path = permutation;
             shortest_distance = current_shortest_distance;
-
         }
+    }
+
+    (shortest_path, shortest_distance)
+}
+
+fn brute_force_tsp_w_threads(distance_matrix: Arc<Vec<Vec<f32>>>) -> (Vec<usize>, f32) {   
+    let shared_queue: Arc<Mutex<VecDeque<Option<Vec<usize>>>>> = Arc::new(Mutex::new(VecDeque::new()));
+    let (tx, rx) = mpsc::channel();
+    let mut handles = vec![];
+
+    for _ in 0..MAX_THREADS {
+        let shared_queue = Arc::clone(&shared_queue);
+        let tx = tx.clone();
+        let distance_matrix = Arc::clone(&distance_matrix);
+        let handle = thread::spawn(move || {
+            loop {
+                let permutation;
+                {
+                    let mut queue = shared_queue.lock().unwrap();
+                    permutation = (*queue).pop_front();
+                }
+
+                let permutation = match permutation {
+                    Some(x) => x,
+                    None => {
+                        println!("Zzzzzz... {:?}", thread::current().id());
+                        thread::sleep(Duration::from_millis(500));
+                        continue
+                    }
+                };
+                
+                match permutation {
+                    None => return,
+                    Some(permutation) => {
+                        let mut current_shortest_distance = 0.0;
+                        for position in 0..permutation.len() - 1 {
+                            current_shortest_distance += distance_matrix[permutation[position]][permutation[position + 1]];
+                        }
+                        current_shortest_distance += distance_matrix[permutation[permutation.len() - 1]][permutation[0]];
+
+                        tx.send((permutation, current_shortest_distance)).unwrap();
+                    }
+                }
+            } 
+        });
+        handles.push(handle);
+    }
+    drop(tx);
+    
+    let start = Instant::now();
+    for permutation in (0..distance_matrix.len()).permutations(distance_matrix.len()) {
+        let mut queue = shared_queue.lock().unwrap();
+        (*queue).push_back(Some(permutation));        
+    }
+    let duration = start.elapsed();
+    println!("Execution time new permutations: {:?}", duration);
+
+    for _ in 0..MAX_THREADS {
+        let mut queue = shared_queue.lock().unwrap();
+        (*queue).push_back(None);       
+    }
+
+    let mut shortest_path: Vec<usize> = vec![];
+    let mut shortest_distance = f32::MAX;
+
+    for msg in rx {
+        if msg.1 < shortest_distance {
+            shortest_path = msg.0;
+            shortest_distance = msg.1;
+        }
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
     }
 
     (shortest_path, shortest_distance)
@@ -71,8 +152,17 @@ fn main() {
 
     // println!("{:?}", distance_matrix);
 
+    let start = Instant::now();
     let (shortest_path, shortest_distance) = brute_force_tsp(&distance_matrix);
+    let duration = start.elapsed();
     println!("Brute force");
-    println!("{:?}", shortest_path);
-    println!("{}", shortest_distance);
+    println!("{:?}: {}", shortest_path, shortest_distance);
+    println!("Execution time: {:?}", duration);
+
+    let start = Instant::now();
+    let (shortest_path, shortest_distance) = brute_force_tsp_w_threads(Arc::new(distance_matrix));
+    let duration = start.elapsed();
+    println!("Brute force with threads");
+    println!("{:?}: {}", shortest_path, shortest_distance);
+    println!("Execution time: {:?}", duration);
 }
