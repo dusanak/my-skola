cc637e1b9054805c377d1f4870e966f07204b36a
Dusan J <dusan.janis97@gmail.com>
2021-12-07 17:33:20 +0100

    PVOS added pr12 and non functional cviceni12.

---
+++ pvos-ukoly/cviceni12/socket_srv.cpp
@@ -0,0 +1,366 @@
+//***************************************************************************
+//
+// Program example for labs in subject Operating Systems
+//
+// Petr Olivka, Dept. of Computer Science, petr.olivka@vsb.cz, 2017
+//
+// Example of socket server.
+//
+// This program is example of socket server and it allows to connect and serve
+// the only one client.
+// The mandatory argument of program is port number for listening.
+//
+//***************************************************************************
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <poll.h>
+#include <sys/socket.h>
+#include <sys/param.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <openssl/rsa.h>       /* SSLeay stuff */
+#include <openssl/crypto.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#define STR_CLOSE   "close"
+#define STR_QUIT    "quit"
+
+//***************************************************************************
+// log messages
+
+#define LOG_ERROR               0       // errors
+#define LOG_INFO                1       // information and notifications
+#define LOG_DEBUG               2       // debug messages
+
+/* define HOME to be dir for key and cert files... */
+#define HOME "./"
+/* Make these what you want for cert & key files */
+#define CERTF  HOME "my.crt"
+#define KEYF  HOME  "my.key"
+
+
+#define CHK_NULL(x) if ((x)==NULL) exit (1)
+#define CHK_ERR(err,s) if ((err)==-1) { perror(s); exit(1); }
+#define CHK_SSL(err) if ((err)==-1) { ERR_print_errors_fp(stderr); exit(2); }
+
+// debug flag
+int g_debug = LOG_INFO;
+
+void log_msg( int t_log_level, const char *t_form, ... )
+{
+    const char *out_fmt[] = {
+            "ERR: (%d-%s) %s\n",
+            "INF: %s\n",
+            "DEB: %s\n" };
+
+    if ( t_log_level && t_log_level > g_debug ) return;
+
+    char l_buf[ 1024 ];
+    va_list l_arg;
+    va_start( l_arg, t_form );
+    vsprintf( l_buf, t_form, l_arg );
+    va_end( l_arg );
+
+    switch ( t_log_level )
+    {
+    case LOG_INFO:
+    case LOG_DEBUG:
+        fprintf( stdout, out_fmt[ t_log_level ], l_buf );
+        break;
+
+    case LOG_ERROR:
+        fprintf( stderr, out_fmt[ t_log_level ], errno, strerror( errno ), l_buf );
+        break;
+    }
+}
+
+//***************************************************************************
+// help
+
+void help( int t_narg, char **t_args )
+{
+    if ( t_narg <= 1 || !strcmp( t_args[ 1 ], "-h" ) )
+    {
+        printf(
+            "\n"
+            "  Socket server example.\n"
+            "\n"
+            "  Use: %s [-h -d] port_number\n"
+            "\n"
+            "    -d  debug mode \n"
+            "    -h  this help\n"
+            "\n", t_args[ 0 ] );
+
+        exit( 0 );
+    }
+
+    if ( !strcmp( t_args[ 1 ], "-d" ) )
+        g_debug = LOG_DEBUG;
+}
+
+//***************************************************************************
+
+int main( int t_narg, char **t_args )
+{
+    if ( t_narg <= 1 ) help( t_narg, t_args );
+
+    int l_port = 0;
+
+    // parsing arguments
+    for ( int i = 1; i < t_narg; i++ )
+    {
+        if ( !strcmp( t_args[ i ], "-d" ) )
+            g_debug = LOG_DEBUG;
+
+        if ( !strcmp( t_args[ i ], "-h" ) )
+            help( t_narg, t_args );
+
+        if ( *t_args[ i ] != '-' && !l_port )
+        {
+            l_port = atoi( t_args[ i ] );
+            break;
+        }
+    }
+
+    if ( l_port <= 0 )
+    {
+        log_msg( LOG_INFO, "Bad or missing port number %d!", l_port );
+        help( t_narg, t_args );
+    }
+
+    log_msg( LOG_INFO, "Server will listen on port: %d.", l_port );
+
+    SSL_CTX* ctx;
+    SSL*     ssl;
+    const SSL_METHOD *meth;
+
+    SSL_load_error_strings();
+    SSLeay_add_ssl_algorithms();
+    meth = SSLv23_server_method();
+    ctx = SSL_CTX_new (meth);
+    if (!ctx) {
+        ERR_print_errors_fp(stderr);
+        exit(2);
+    }
+    
+    if (SSL_CTX_use_certificate_file(ctx, CERTF, SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(3);
+    }
+    if (SSL_CTX_use_PrivateKey_file(ctx, KEYF, SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(4);
+    }
+
+    if (!SSL_CTX_check_private_key(ctx)) {
+        fprintf(stderr,"Private key does not match the certificate public key\n");
+        exit(5);
+    }
+
+    // socket creation
+    int l_sock_listen = socket( AF_INET, SOCK_STREAM, 0 );
+    if ( l_sock_listen == -1 )
+    {
+        log_msg( LOG_ERROR, "Unable to create socket.");
+        exit( 1 );
+    }
+
+    in_addr l_addr_any = { INADDR_ANY };
+    sockaddr_in l_srv_addr;
+    l_srv_addr.sin_family = AF_INET;
+    l_srv_addr.sin_port = htons( l_port );
+    l_srv_addr.sin_addr = l_addr_any;
+
+
+    // Enable the port number reusing
+    int l_opt = 1;
+    if ( setsockopt( l_sock_listen, SOL_SOCKET, SO_REUSEADDR, &l_opt, sizeof( l_opt ) ) < 0 )
+      log_msg( LOG_ERROR, "Unable to set socket option!" );
+
+    // assign port number to socket
+    if ( bind( l_sock_listen, (const sockaddr * ) &l_srv_addr, sizeof( l_srv_addr ) ) < 0 )
+    {
+        log_msg( LOG_ERROR, "Bind failed!" );
+        close( l_sock_listen );
+        exit( 1 );
+    }
+
+    // listenig on set port
+    if ( listen( l_sock_listen, 1 ) < 0 )
+    {
+        log_msg( LOG_ERROR, "Unable to listen on given port!" );
+        close( l_sock_listen );
+        exit( 1 );
+    }
+
+    log_msg( LOG_INFO, "Enter 'quit' to quit server." );
+
+    // go!
+    while ( 1 )
+    {
+        int l_sock_client = -1;
+
+        // list of fd sources
+        pollfd l_read_poll[ 2 ];
+
+        l_read_poll[ 0 ].fd = STDIN_FILENO;
+        l_read_poll[ 0 ].events = POLLIN;
+        l_read_poll[ 1 ].fd = l_sock_listen;
+        l_read_poll[ 1 ].events = POLLIN;
+
+        while ( 1 ) // wait for new client
+        {
+            // select from fds
+            int l_poll = poll( l_read_poll, 2, -1 );
+
+            if ( l_poll < 0 )
+            {
+                log_msg( LOG_ERROR, "Function poll failed!" );
+                exit( 1 );
+            }
+
+            if ( l_read_poll[ 0 ].revents & POLLIN )
+            { // data on stdin
+                char buf[ 128 ];
+                int len = read( STDIN_FILENO, buf, sizeof( buf) );
+                if ( len < 0 )
+                {
+                    log_msg( LOG_DEBUG, "Unable to read from stdin!" );
+                    exit( 1 );
+                }
+
+                log_msg( LOG_DEBUG, "Read %d bytes from stdin" );
+                // request to quit?
+                if ( !strncmp( buf, STR_QUIT, strlen( STR_QUIT ) ) )
+                {
+                    log_msg( LOG_INFO, "Request to 'quit' entered.");
+                    close( l_sock_listen );
+                    exit( 0 );
+                }
+            }
+
+            if ( l_read_poll[ 1 ].revents & POLLIN )
+            { // new client?
+                sockaddr_in l_rsa;
+                int l_rsa_size = sizeof( l_rsa );
+                // new connection
+                l_sock_client = accept( l_sock_listen, ( sockaddr * ) &l_rsa, ( socklen_t * ) &l_rsa_size );
+                if ( l_sock_client == -1 )
+                {
+                        log_msg( LOG_ERROR, "Unable to accept new client." );
+                        close( l_sock_listen );
+                        exit( 1 );
+                }
+                uint l_lsa = sizeof( l_srv_addr );
+                // my IP
+                getsockname( l_sock_client, ( sockaddr * ) &l_srv_addr, &l_lsa );
+                log_msg( LOG_INFO, "My IP: '%s'  port: %d",
+                                 inet_ntoa( l_srv_addr.sin_addr ), ntohs( l_srv_addr.sin_port ) );
+                // client IP
+                getpeername( l_sock_client, ( sockaddr * ) &l_srv_addr, &l_lsa );
+                log_msg( LOG_INFO, "Client IP: '%s'  port: %d",
+                                 inet_ntoa( l_srv_addr.sin_addr ), ntohs( l_srv_addr.sin_port ) );
+
+                
+                ssl = SSL_new (ctx);                           CHK_NULL(ssl);
+                SSL_set_fd (ssl, l_sock_client);
+                int err = SSL_accept (ssl);                        CHK_SSL(err);
+                
+                break;
+            }
+
+        } // while wait for client
+
+        // change source from sock_listen to sock_client
+        l_read_poll[ 1 ].fd = l_sock_client;
+
+        while ( 1 )
+        { // communication
+            char l_buf[ 256 ];
+
+            // select from fds
+            int l_poll = poll( l_read_poll, 2, -1 );
+
+            if ( l_poll < 0 )
+            {
+                log_msg( LOG_ERROR, "Function poll failed!" );
+                exit( 1 );
+            }
+
+            // data on stdin?
+            if ( l_read_poll[ 0 ].revents & POLLIN )
+            {
+                // read data from stdin
+                int l_len = read( STDIN_FILENO, l_buf, sizeof( l_buf ) );
+                if ( l_len < 0 )
+                        log_msg( LOG_ERROR, "Unable to read data from stdin." );
+                else
+                        log_msg( LOG_DEBUG, "Read %d bytes from stdin.", l_len );
+
+                // send data to client
+                l_len = SSL_write (ssl, l_buf, l_len);  CHK_SSL(l_len);
+                if ( l_len < 0 )
+                        log_msg( LOG_ERROR, "Unable to send data to client." );
+                else
+                        log_msg( LOG_DEBUG, "Sent %d bytes to client.", l_len );
+            }
+            // data from client?
+            if ( l_read_poll[ 1 ].revents & POLLIN )
+            {
+                // read data from socket
+                int l_len = SSL_read (ssl, l_buf, sizeof(l_buf) - 1);  CHK_SSL(l_len);
+                l_buf[l_len] = '\0';
+                printf ("Got %d chars:'%s'\n", l_len, l_buf);
+                if ( !l_len )
+                {
+                        log_msg( LOG_DEBUG, "Client closed socket!" );
+                        close( l_sock_client );
+                        break;
+                }
+                else if ( l_len < 0 )
+                        log_msg( LOG_DEBUG, "Unable to read data from client." );
+                else
+                        log_msg( LOG_DEBUG, "Read %d bytes from client.", l_len );
+
+                // write data to client
+                l_len = write( STDOUT_FILENO, l_buf, l_len );
+                if ( l_len < 0 )
+                        log_msg( LOG_ERROR, "Unable to write data to stdout." );
+
+                // close request?
+                if ( !strncasecmp( l_buf, "close", strlen( STR_CLOSE ) ) )
+                {
+                        log_msg( LOG_INFO, "Client sent 'close' request to close connection." );
+                        close( l_sock_client );
+                        log_msg( LOG_INFO, "Connection closed. Waiting for new client." );
+                        break;
+                }
+            }
+            // request for quit
+            if ( !strncasecmp( l_buf, "quit", strlen( STR_QUIT ) ) )
+            {
+                close( l_sock_listen );
+                close( l_sock_client );
+                SSL_free (ssl);
+                SSL_CTX_free (ctx);
+                log_msg( LOG_INFO, "Request to 'quit' entered" );
+                exit( 0 );
+            }
+        } // while communication
+    } // while ( 1 )
+
+    return 0;
+}
